06502fbf0453abac08a83a050beebe0c694ca19a
weasy1as <habdu21@student.sdu.dk>
2025-09-16 14:17:10 +0200

    first commit

---
+++ backend/src/main/java/com/weasy/aijobmatch/AijobmatchApplication.java
@@ -0,0 +1,13 @@
+package com.weasy.aijobmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AijobmatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AijobmatchApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/weasy/aijobmatch/AijobmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weasy.aijobmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AijobmatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
